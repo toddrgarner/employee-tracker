1b7b5e2301c54d727f3a2fa09be4ed39ee49a9c2
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-09-24 02:01:19 -0700

    added server.js, inquirer 89.2.45 and mysql2

---
+++ server.js
@@ -0,0 +1,6 @@
+// ************* Reference Variables ************* //
+const express = require('express');
+const Directory = require("./lib/index.js");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
